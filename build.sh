f4d7431174bdac29242ccc671c87a53fac0daa79
Jack May <jack@solana.com>
2019-02-19 15:11:42 -0800

    Update build script to set toolchain and install xargo

--- build.sh
+++ build.sh
@@ -1,13 +1,21 @@
 #!/usr/bin/env bash
 
 set -x
+
+cargo install xargo
+
 set -e
 
-rustup default bpf2
-export XARGO_HOME=./target/rust-sysroot
-export XARGO_RUST_SRC=/Users/jack/workbench/hack/rust_bpf_sysroot/rust-src
+rustup override set bpf
+
+git submodule init
+git submodule update
+
+export XARGO_HOME="$PWD/target/rust-sysroot"
+export XARGO_RUST_SRC="$PWD/src"
 xargo build --target bpfel_unknown_unknown --release -v
 
-cp -rf ./target/rust-sysroot ../../active/solana/sdk/bpf/
+# Don't need x86 stuff
+rm -rf ./target/rust-sysroot/lib/rustlib/x86_64-apple-darwin
 
 { { set +x; } 2>/dev/null; echo Success; }
