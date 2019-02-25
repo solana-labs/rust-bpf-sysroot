00d0375368bbd9722717721860f7d3e84f906704
Jack May <jack@solana.com>
2019-02-25 11:14:51 -0800

    Download and install rust-bpf toolchain, create release tar

--- build.sh
+++ build.sh
@@ -1,12 +1,45 @@
 #!/usr/bin/env bash
 
-set -x
+set -ex
 
+if [[ "$(uname)" = Darwin ]]; then
+  machine=osx
+  triple=x86_64-apple-darwin
+else
+  machine=linux
+  triple=x86_64-unknown-linux-gnu
+fi
+
+# Install Rust BPF
+version=v0.0.1
+if [[ ! -f rust-bpf-$machine-$version.md ]]; then
+  (
+    filename=solana-rust-bpf-$machine.tar.bz2
+
+    set -ex
+    rm -rf rust-bpf*
+    mkdir -p rust-bpf
+    pushd rust-bpf
+    wget --progress=dot:giga https://github.com/solana-labs/rust-bpf-builder/releases/download/$version/$filename
+    # cp ../../../hack/rust-bpf-builder/deploy/$filename .
+    tar -jxf $filename
+    rm -rf $filename
+    echo "https://github.com/solana-labs/rust-bpf-builder/releases/tag/$version" > ../rust-bpf-$machine-$version.md
+    popd
+  )
+  exitcode=$?
+  if [[ $exitcode -ne 0 ]]; then
+    rm -rf rust-bpf
+    exit 1
+  fi
+fi
+
+set +e
 cargo install xargo
-
+rustup toolchain uninstall bpfsysroot
 set -e
-
-rustup override set bpf
+rustup toolchain link bpfsysroot rust-bpf
+rustup override set bpfsysroot
 
 git submodule init
 git submodule update
@@ -16,6 +49,11 @@ export XARGO_RUST_SRC="$PWD/src"
 xargo build --target bpfel_unknown_unknown --release -v
 
 # Don't need x86 stuff
-rm -rf ./target/rust-sysroot/lib/rustlib/x86_64-apple-darwin
+rm -rf ./target/rust-sysroot/lib/rustlib/"$triple"
+
+# Tar for release
+pushd target
+tar -C ./rust-sysroot -jcf solana-rust-bpf-sysroot.tar.bz2 .
+popd
 
 { { set +x; } 2>/dev/null; echo Success; }
