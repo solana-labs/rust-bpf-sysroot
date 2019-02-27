d07e327944da71e3d932d5eb5ed3ba6e56738392
Jack May <jack@solana.com>
2019-02-27 15:24:53 -0800

    Use built-in rustc target

--- build.sh
+++ build.sh
@@ -12,14 +12,14 @@ fi
 
 # Install LLVM
 version=v0.0.8
-if [[ ! -f llvm-native-$machine-$version.md ]]; then
+if [[ ! -f deps/llvm-native-$machine-$version.md ]]; then
   (
     filename=solana-llvm-$machine.tar.bz2
 
     set -ex
-    rm -rf llvm-native*
-    mkdir -p llvm-native
-    cd llvm-native
+    rm -rf deps/llvm-native*
+    mkdir -p deps/llvm-native
+    cd deps/llvm-native
     wget --progress=dot:giga https://github.com/solana-labs/llvm-builder/releases/download/$version/$filename
     tar -jxf $filename
     rm -rf $filename
@@ -35,30 +35,26 @@ fi
 
 # Install Rust BPF
 version=v0.0.1
-if [[ ! -f rust-bpf-$machine-$version.md ]]; then
+if [[ ! -f deps/rust-bpf-$machine-$version.md ]]; then
   (
     filename=solana-rust-bpf-$machine.tar.bz2
 
     set -ex
-    rm -rf rust-bpf
-    rm -rf rust-bpf-$machine-*
-    mkdir -p rust-bpf
-    pushd rust-bpf
+    rm -rf deps/rust-bpf
+    rm -rf deps/rust-bpf-$machine-*
+    mkdir -p deps/rust-bpf
+    pushd deps/rust-bpf
     wget --progress=dot:giga https://github.com/solana-labs/rust-bpf-builder/releases/download/$version/$filename
     tar -jxf $filename
     rm -rf $filename
-    popd
-
-    set -ex
-    ./rust-bpf/bin/rustc --print sysroot
 
     set +e
     rustup toolchain uninstall bpfsysroot
     set -e
-    rustup toolchain link bpfsysroot rust-bpf
+    rustup toolchain link bpfsysroot ../rust-bpf
     rustup override set bpfsysroot
 
-    echo "https://github.com/solana-labs/rust-bpf-builder/releases/tag/$version" > rust-bpf-$machine-$version.md
+    echo "https://github.com/solana-labs/rust-bpf-builder/releases/tag/$version" > ../rust-bpf-$machine-$version.md
   )
   exitcode=$?
   if [[ $exitcode -ne 0 ]]; then
@@ -74,16 +70,17 @@ set -e
 git submodule init
 git submodule update
 
-export XARGO_HOME="$PWD/target/rust-sysroot"
+export RUSTFLAGS="$RUSTFLAGS \
+    -C lto=no -C opt-level=2 \
+    -C link-arg=-Tbpf.ld \
+    -C link-arg=-z -C link-arg=notext \
+    -C link-arg=--Bdynamic \
+    -C link-arg=-shared \
+    -C link-arg=--entry=entrypoint \
+    -C linker=deps/llvm-native/bin/ld.lld"
+
+export XARGO_HOME="$PWD/target/xargo"
 export XARGO_RUST_SRC="$PWD/src"
-xargo build --target bpfel_unknown_unknown --release -v
-
-# Don't need native target stuff
-rm -rf ./target/rust-sysroot/lib/rustlib/"$triple"
-
-# Tar for release
-pushd target
-tar -C ./rust-sysroot -jcf solana-rust-bpf-sysroot.tar.bz2 .
-popd
+xargo build --target bpfel-unknown-unknown --release -v
 
 { { set +x; } 2>/dev/null; echo Success; }
