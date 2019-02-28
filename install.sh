03f427aedcee7f701dbec504d26c5bae62917568
Jack May <jack@solana.com>
2019-02-28 00:05:03 -0800

    remove unused script

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -x
-set -e
-
-rm -rf ../../active/solana/sdk/bpf/rust-sysroot
-cp -rf ./target/rust-sysroot/ ../../active/solana/sdk/bpf/rust-bpf-sysroot
-
-{ { set +x; } 2>/dev/null; echo Success; }
